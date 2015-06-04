a2faaaf616eef2529d5e810c896fdba0d31524ac
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2015-06-04 14:19:30 +0000

    add system support to cumulus provider

--- netshow/cumulus/show_system.py
+++ netshow/cumulus/show_system.py
@@ -1,15 +1,51 @@
 """ Module for Managing switch system information printout
 """
 
-class ShowSystem(object):
+from netshowlib.cumulus.system_summary import SystemSummary
+from netshow.linux import show_system as linux_system
+from flufl.i18n import initialize
+
+_ = initialize('netshow-linux-lib')
+
+
+class ShowSystem(linux_system.ShowSystem):
     """
     Class responsible for printing out basic switch summary info
+    for the cumulus provider
     """
     def __init__(self, **kwargs):
-        pass
+        self.use_json = kwargs.get('--json') or kwargs.get('-j')
+        self.system = SystemSummary()
 
-    def run(self):
+
+    def cli_output(self):
         """
-        :return: output regarding system like OS type, etc
+        :return: print out system info on a cumulus switch
         """
-        pass
+
+        pd = self.system.platform_info
+        # If platform detect does not work.
+        # TODO make this better....
+        if not pd:
+            return super(ShowSystem, self).cli_output()
+
+        chip = pd.get('chipset')
+        cpu = pd.get('processor')
+        _str = ''
+        _str = "\n%s %s\n" % (pd.get('manufacturer'), pd.get('model'))
+        _str += "%s %s %s\n" % (self.system.os_name, _('version'),
+                                self.system.version)
+        _str += "%s: %s\n\n" % (_('build'), self.system.os_build)
+        _str += "%s: %s %s %s\n\n" % (_('chipset'),
+                                      chip.get('manufacturer'),
+                                      chip.get('family'),
+                                      chip.get('model'))
+        _str += "%s: %s\n\n" % (_('port config'), pd.get('ports'))
+        _str += "%s: (%s) %s %s %s %s\n\n" % (_('cpu'),
+                                              self.system.arch,
+                                              cpu.get('manufacturer'),
+                                              cpu.get('family'),
+                                              cpu.get('model'),
+                                              cpu.get('speed'))
+        _str += "%s: %s\n" % (_('uptime'), self.uptime)
+        return _str
