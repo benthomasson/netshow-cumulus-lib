09f174b435e7c4aeb44488cf11c9f9a973d10ca2
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2015-06-29 17:10:09 +0000

    make netshow cumulus show_neighbors module work with reusable linux provider show_neighbors module

--- netshow/cumulus/show_neighbors.py
+++ netshow/cumulus/show_neighbors.py
@@ -10,26 +10,12 @@
 
 import netshow.linux.show_neighbors as linux_showneighbors
 import netshowlib.cumulus.cache as cumulus_cache
-import json
-from netshow.linux.netjson_encoder import NetEncoder
-from netshow.cumulus import print_iface
 
 
 class ShowNeighbors(linux_showneighbors.ShowNeighbors):
     """
     Class responsible for printing out basic cumulus device neighbor info
     """
-    def run(self):
-        """
-        :return: basic neighbor information based on data obtained on netshow-lib
-        """
-        feature_cache = cumulus_cache.Cache()
-        feature_cache.run()
-        for _ifacename in feature_cache.lldp.keys():
-            self.ifacelist[_ifacename] = print_iface.iface(_ifacename, feature_cache)
-
-        if self.use_json:
-            return json.dumps(self.ifacelist,
-                              cls=NetEncoder, indent=4)
-
-        return self.print_neighbor_info()
+    def __init__(self, cl):
+        linux_showneighbors.ShowNeighbors.__init__(self, cl)
+        self.cache = cumulus_cache
